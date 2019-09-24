85fd199a542bd1d1b5da456904403a8217be22a2
nandohidayat <mnandohidayat@gmail.com>
2019-09-24 15:27:01 +0700

    add eventListener

---
+++ main.js
@@ -0,0 +1,30 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    image: "./assets/vmSocks-green.jpg",
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green.jpg"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue.jpg"
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart: function() {
+      this.cart++;
+    },
+    updateProduct: function(variantImage) {
+      this.image = variantImage;
+    }
+  }
+});
